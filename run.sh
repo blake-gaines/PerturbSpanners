50ba07a30941eaf1d62e9b6470ed09d565aa7776
Blake Gaines <blake@uconn.edu>
2023-01-18 12:32:32 -0500

    Clean up run.sh

--- run.sh
+++ run.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
-##SBATCH --account=jib10001 --partition=GpuPriorityJB --qos=jib10001gpu
 #SBATCH --nodes=1                  
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
-##SBATCH --mem-per-cpu=2G
 #SBATCH --gres=gpu:0
-##SBATCH --exclude=gpu[01-08],gpu[10],gtx[01-16]
 #SBATCH --error=output.log
 #SBATCH --output=output.log
 ## SBATCH --time=5-00:00:00
-##SBATCH --mail-user=blake@uconn.edu
-##SBATCH --mail-type=BEGIN
 
 module load cuda/10.1
 
